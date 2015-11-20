5c876014739c196650ce252951bf8206d775e7ed
rajan <rajan.inception.software@gmail.com>
2015-11-21 10:53:30 +1100

    added yii2 framework

---
+++ app/controllers/CommentController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rajan
+ * Date: 21/11/2015
+ * Time: 10:35 AM
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class CommentController extends Controller{
+
+    public function actionIndex(){
+        echo "This is a default controller";
+    }
+}
